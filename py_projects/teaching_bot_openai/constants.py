891396a1025e26e9c6755dde38be9c99824601da
Yajing Liu <yajing.leo@gmail.com>
2025-08-02 04:27:00 +0000

    Delete all messages

--- py_projects/teaching_bot_openai/constants.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun  9 22:49:27 2024
-
-@author: yajingliu
-"""
-
-API_KEY = "sk-proj-SI7NJ53KdgBHArliEi4DT3BlbkFJdBYDW4TB3HeMAvbt0w6L"
